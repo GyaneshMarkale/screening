4347f5db13ea3a086f4b1d74225e90f82595ac4f
Gyanesh Markale <31774714+GyaneshMarkale@users.noreply.github.com>
2021-03-31 19:08:21 +0530

    Screening test
    Make a message scheduling program which takes in a number of messages sent by
    users and schedules them in uniform distribution. For example take a list of users with
    messages
    A. 3
    B. 2
    C. 1
    For the users A, B, and C, with message count 3, 2 and 1 respectively; The messages
    can get scheduled in sequence ‘ABCABA’, note that there may be more than one
    possible sequence or in some cases no perfect sequence..
    The program should be able to parse a CSV of input messages and generate the
    sequence of message schedule as uniformly distributed as possible.
    
    
    
    Solution;
    
    1. read the csv
    2. convert  data in map(key, value) pair
    3. calculate how many time its should run(how many message to print) by calculating the total length of message.
    4. iterate over the total length, and inner loop iterate over the map.
    5. In inner loop map is iterate in which if encounter with key value 0 the donot print else add stringbuilder object and decrement its value by 1
    6. finally return message in the form of string.

---
+++ testScreening.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+import org.supercsv.io.CsvListReader;
+import org.supercsv.prefs.CsvPreference;
+
+public class Test {
+
+
+  public static void main(String[] args) throws IOException {
+    /**
+     * Load CSV from classpath
+     */
+    CsvListReader csvReader = new CsvListReader(new InputStreamReader(
+        ReadCSVAsListOfMaps.class.getClassLoader().getResourceAsStream("test_sceening.csv")),
+        CsvPreference.STANDARD_PREFERENCE);
+    
+    //Read CSV Header
+    List<String> header = new ArrayList<String>(csvReader.read());
+      List<String> rowData;
+      
+      //Read the CSV as List of Maps where each Map represents row data
+      List<Map<String, String>> rows = new ArrayList<Map<String, String>>();
+      Map<String, Intger> map = null;
+      
+      while ((rowData = csvReader.read()) != null) {
+        //Create Map for each row in CSV
+        map = new HashMap<String, Intger>();
+        
+        for (int i = 0 ; i < header.size() ; i ++) {
+          map.put(header.get(i), rowData.get(i));
+        }
+        
+        //add Row map to list of rows
+        map.add(row);
+      }
+      
+      
+      //Iterate
+      //for (Map<String, String> rowMap : rows) {
+      //  System.out.println(rowMap);
+      //}
+      res = messageDecode(map);
+      Syetem.out.println(res);
+      
+  }
+  public String messageDecode(Map<String, Intger> map){
+    StringBuilder sb = new StringBuilder();
+    int num = 0;
+    it count = 0;
+    for(Map.Entry<String,Integer> entry1 :: map.entrySet()){
+      num = entry1.getValue();
+      count += num;
+     }
+     for(int i = 0; i < count; i++){
+       for(Map.Entry<String,Integer> entry2 :: map.entrySet()){
+        if (entry2.getValue() == 0) continue;
+        else{
+          sb.append(entry2.getKey());
+          map.put(entry.getKey(), entry2.getValue()-1)
+        }
+          
+       }
+     }
+     return sb.toString();      
+  }
+}
